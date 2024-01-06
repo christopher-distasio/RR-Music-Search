923be0871803219de0e7d35334f1739d9643d25d
Chris DiStasio <stahzz1970@gmail.com>
2024-01-06 09:51:17 -0600

    Completes ArtistView.js

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,40 +1,55 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 function ArtistView() {
-    const { id } = useParams()
-    const [ artistData, setArtistData ] = useState([])
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const API_URL = `http://localhost:4000/album/${id}`
-        const fetchData = async () => {
-            const response = await fetch(API_URL)
-            const resData = await response.json()
-            setArtistData(resData.results)
-        }
-        fetchData()
-    }, [id])
+  const { id } = useParams();
+  const [artistData, setArtistData] = useState([]);
 
-    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
-
-    const renderAlbums = justAlbums.map((album, i) => {
-        return (
-            <div key={i}>
-              {/* The key property is missing the {i} */}
-                <Link to={`/album/${album.collectionId}`}>
-                    <p>{album.collectionName}</p>
-                </Link>
-                {/* This final tag is missing the forward slash in source code */}
-            </div>
-        )
-    })
-
+  const navButtons = () => {
     return (
-        <div>
-            {renderAlbums}
-        </div>
-    )
+      <div>
+        <button onClick={() => navigate(-1)}>Back</button>
+        <button onClick={() => navigate("/")}>Home</button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    const API_URL = `http://localhost:4000/album/${id}`;
+    const fetchData = async () => {
+      const response = await fetch(API_URL);
+      const resData = await response.json();
+      setArtistData(resData.results);
+    };
+    fetchData();
+  }, [id]);
+
+  const justAlbums = artistData.filter(
+    (entry) => entry.collectionType === "Album"
+  );
+
+  const renderAlbums = justAlbums.map((album, i) => {
+    return (
+      <div key={i}>
+        {/* The key property is missing the {i} */}
+        <Link to={`/album/${album.collectionId}`}>
+          <p>{album.collectionName}</p>
+        </Link>
+        {/* This final tag is missing the forward slash in source code */}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>{artistData.length ? artistData[0].artistName : "Loading..."}</h1>{" "}
+      {navButtons()}
+      {renderAlbums}
+    </div>
+  );
 }
 
-export default ArtistView
+export default ArtistView;
